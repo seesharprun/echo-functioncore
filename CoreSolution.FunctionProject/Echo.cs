3f0c5389453e6fde3e67039730740148b4e01fef
Sidney Andrews <sidney@seesharprun.net>
2018-02-18 20:52:03 -0500

    Initial Commit

---
+++ CoreSolution.FunctionProject/Echo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs.Host;
+using System.Linq;
+using Microsoft.AspNetCore.SignalR.Client;
+using System.Threading.Tasks;
+
+namespace CoreSolution.FunctionProject
+{
+    public class Echo
+    {
+        public static IActionResult Run(HttpRequest request, TraceWriter log)
+        {
+            log.Info("C# HTTP trigger function processed a request.");
+
+            if (request.Query.TryGetValue("name", out StringValues value)) {
+                return new OkObjectResult($"Hello, {value.First()}");
+            }
+        }
+    }
+}
